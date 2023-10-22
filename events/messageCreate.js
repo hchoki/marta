726b1be883506c876cbef09f36c676be1a60b5a1
choki <choki@pawlygon.net>
2023-10-21 23:00:32 -0400

    eslint fix

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,15 +1,15 @@
 module.exports = (client, message) => {
-  if (message.author.bot) return;
+	if (message.author.bot) return;
 
-  const linkRegex = /https?:\/\/(www\.)?(twitter\.com|x\.com)\/[^\s]*/g;
-  const foundLinks = message.content.match(linkRegex);
+	const linkRegex = /https?:\/\/(www\.)?(twitter\.com|x\.com)\/[^\s]*/g;
+	const foundLinks = message.content.match(linkRegex);
 
-  if (message.embeds.length === 0 && foundLinks) {
-    let replyText = '';
-    for (const link of foundLinks) {
-      const convertedLink = link.replace(/(twitter\.com|x\.com)/, 'vxtwitter.com');
-      replyText += `${convertedLink}\n`;
-    }
-    message.reply(replyText);
-  }
+	if (message.embeds.length === 0 && foundLinks) {
+		let replyText = '';
+		for (const link of foundLinks) {
+			const convertedLink = link.replace(/(twitter\.com|x\.com)/, 'vxtwitter.com');
+			replyText += `${convertedLink}\n`;
+		}
+		message.reply(replyText);
+	}
 };
