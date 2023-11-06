c47c296e095deea710370579c72dd711eda1c87a
choki <choki@pawlygon.net>
2023-11-05 22:41:36 -0500

    Fix dx2

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -15,7 +15,7 @@ module.exports = (client, message) => {
 	}
 
 	// VR message logic
-	const vrRelatedWords = /valve|deckard|quest|vr|pico/i;
+	const vrRelatedWords = /valve|deckard|quest|\bvr\b|pico/i;
 	if (vrRelatedWords.test(message.content)) {
 		message.reply(getMessage('2023-12-04T18:00:00'));
 	}
