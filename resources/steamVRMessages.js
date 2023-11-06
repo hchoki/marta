78ab11ab2b21359e34f421001ff74807157234d7
choki <choki@pawlygon.net>
2023-11-05 22:44:31 -0500

    Test

--- resources/steamVRMessages.js
+++ resources/steamVRMessages.js
@@ -17,26 +17,7 @@ function getTimeLeft(eventDate) {
 module.exports = (eventDate) => {
 	const { daysLeft, hoursLeft, minutesLeft } = getTimeLeft(eventDate);
 	const messages = [
-		`Falta apenas ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o início do Steam VR Festival! A ansiedade está a mil! 🎉`,
-		`Está chegando! Steam VR Festival em ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos! Prepare seu headset! 🕶️`,
-		`A realidade virtual nunca mais será a mesma! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🎮`,
-		`Contagem regressiva: ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos e ${minutesLeft} minutos para o Steam VR Festival! Você está pronto? 💻`,
-		`O futuro da VR está batendo à porta! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🚀`,
-		`Prepare-se para uma imersão como nunca antes! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos até o Steam VR Festival! 🎧`,
-		`Falta pouco! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para descobrir as novidades do Steam VR Festival! 🔥`,
-		`Você está tão animado quanto nós? ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🎉`,
-		`A espera está quase acabando! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! ⏰`,
-		`Conte os minutos! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival começar! 🎊`,
-		`Prepare seu setup! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 💻`,
-		`A revolução da realidade virtual está chegando! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🕶️`,
-		`Está quase na hora! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🎮`,
-		`Fique atento! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! ⏳`,
-		`O mundo da VR está prestes a mudar! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🌎`,
-		`Prepare-se para a aventura VR do ano! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🎢`,
-		`Está quase na hora de colocar seu headset! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 🕶️`,
-		`O Steam VR Festival está chegando! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos restantes! 🎮`,
-		`Prepare-se para experiências incríveis em VR! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! 💻`,
-		`A contagem regressiva está quase no fim! ${daysLeft} dias e ${hoursLeft} horas e ${minutesLeft} minutos para o Steam VR Festival! ⏱️`,
+		'https://tenor.com/view/me-waiting-for-valve-deckard-valve-deckard-skeleton-wheelchair-gif-15912588111334799418',
 	];
 
 	const randomIndex = Math.floor(Math.random() * messages.length);
