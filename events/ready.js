51ad2ed709a660a5a9e07554b1508b2cd6892e9d
Choki <136538030+hchoki@users.noreply.github.com>
2026-09-14 14:16:39 -0400

    Update bot presence message and comment out schedule job

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = (client) => {
 	client.user.setPresence({
-		activities: [{ name: 'Só demorou 709 dias para o Valve Frame ser anunciado', type: 4 }],
+		activities: [{ name: 'Só demorou 1015 dias para o Valve Frame ser anunciado', type: 4 }],
 	});
 
 	// Register slash commands
@@ -28,10 +28,10 @@ module.exports = (client) => {
 	client.application.commands.set(commands);
 
 	// Send a message every day at 6 PM
-	schedule.scheduleJob('0 18 * * *', () => {
-		const channel = client.channels.cache.get('1102758639732801586');
-		if (channel) {
-			channel.send(getMessage('2025-11-12T18:00:00'));
-		}
-	});
+	//schedule.scheduleJob('0 18 * * *', () => {
+	//	const channel = client.channels.cache.get('1102758639732801586');
+	//	if (channel) {
+	//		channel.send(getMessage('2025-11-12T18:00:00'));
+	//	}
+	//});
 };
